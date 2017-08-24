d561c914607fc4ddbb0e8d9b97f0b962be83372c
Andreas <andreas.klausen0@gmail.com>
2017-08-24 19:06:14 +0200

    Initial comments for motor directions

--- car/car.ino
+++ car/car.ino
@@ -1,23 +1,52 @@
 const int dummy = 0;
 const int irpin = 11; //Green
-const int in4pin = 8; //Black
-const int in3pin = 7; //Green
-const int enApin = 6; //Purple
-const int enBpin = 5; //Yellow
-const int in2pin = 4; //Orange
-const int in1pin = 2; //Red
+const int dir2PinB = 8; //Black
+const int dir1PinB = 7; //Green
+const int speedPinA = 6; //Purple
+const int speedPinB = 5; //Yellow
+const int dir2PinA = 4; //Orange
+const int dir1PinA = 2; //Red
 
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(enApin, OUTPUT);
-  pinMode(enBpin, OUTPUT);
-  digitalWrite(enApin, HIGH);
-  digitalWrite(enBpin, LOW);
+  pinMode(dir1PinA, OUTPUT);
+  pinMode(dir2PinA, OUTPUT);
+  pinMode(speedPinA, OUTPUT);
+  pinMode(dir1PinB, OUTPUT);
+  pinMode(dir2PinB, OUTPUT);
+  pinMode(speedPinB, OUTPUT);
 }
 
 void loop()
 {
   Serial.print("hei");
+  /*
+  //Forward left wheel
+  digitalWrite(dir1PinA, LOW);
+  digitalWrite(dir2PinA, HIGH);
+  analogWrite(speedPinA, 200);
+  */
+  
+  /*
+  //Backward left wheel
+  digitalWrite(dir1PinA, HIGH);
+  digitalWrite(dir2PinA, LOW);
+  analogWrite(speedPinA, 200);
+  */
+  
+  /*
+  //Forward right wheel
+  digitalWrite(dir1PinB, LOW);
+  digitalWrite(dir2PinB, HIGH);
+  analogWrite(speedPinB, 200);
+  */
+  
+  /*
+  //Backward right wheel
+  digitalWrite(dir1PinB, HIGH);
+  digitalWrite(dir2PinB, LOW);
+  analogWrite(speedPinB, 200);
+  */
 }
