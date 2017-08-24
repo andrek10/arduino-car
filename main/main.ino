f8050cb2fbc7f156aa839571c8260486401e54d3
Andreas <andreas.klausen0@gmail.com>
2017-08-24 18:31:43 +0200

    Add the main arduino file

---
+++ main/main.ino
@@ -0,0 +1,22 @@
+const int irpin = 11; //Green
+const int in4pin = 8; //Black
+const int in3pin = 7; //Green
+const int enApin = 6; //Purple
+const int enBpin = 5; //Yellow
+const int in2pin = 4; //Orange
+const int in1pin = 2; //Red
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(enApin, OUTPUT);
+  pinMode(enBpin, OUTPUT);
+  digitalWrite(enApin, HIGH);
+  digitalWrite(enBpin, LOW);
+}
+
+void loop()
+{
+  Serial.print("hei");
+}
